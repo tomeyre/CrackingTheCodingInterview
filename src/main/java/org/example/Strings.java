3fd462100bf40cf3d2f6df485b4a196447fb6fc8
Thomas Eyre <tom_1237@hotmail.com>
2023-11-14 23:41:54 +0000

    up to solution 1.4

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Strings {
 
-    // first ask if its in ASCII or unicode
-    //ASCII is a 7-bit character set containing 128 characters
-    //As of Unicode version 15.1, there are 149,878 characters with code points, covering 161 modern and historical scripts, as well as multiple symbol sets.
+    // solution 1 -------------------------------------------------------------------------------
+
+
+    // you can argue that the time complexity is O(n) where n equals the length of the string or O(1) as you will never loop through more than 128 times.
+    // the space complexity is O(1)
     public static boolean checkUniqueUsingBooleanArray(String str){
         if(str.length() > 128) return false;
 
@@ -17,5 +21,138 @@ public class Strings {
             charSet[val] = true;
         }
         return true;
+    }
+
+    // assumption the string only contains lowercase letters
+    // how is this reduced space well we use an int instead of an array to hold our current checks
+    // in the unicode table a = 97;
+    // lets say the first letter of the string is c so val will be 99 - 97 = 2;
+    // next line if ((O & 4) > 0) = false, this may seem obvious but this is a bitwise &
+    // for a start how did we get 4? 1 << 2 = 100 and 100 converted from binary to int = 4
+    // the rest of the calculation is 0 & 100 = 000 so it is not greater than 0.
+    // next line 0 |= 4 answer 4
+    // how did we get 4? 0 |= 4 is a bitwise operation 0 in binary is 0 and 4 in binary is 100 the or operation = 100
+    // next letter is d so val = 3;
+    // next line if ((4 & 8) > 0) false
+    // 1 << 3 = 1000 = 8
+    // 100 & 1000 = 0000 so not greater than 0
+    // next line 4 |= 8
+    // 100 |= 1000 = 1100 = 12
+    public static boolean checkUniqueReducedSpaceComplexity(String str){
+        int checker = 0;
+        for(int i = 0; i < str.length(); i++){
+            int val = str.charAt(i) - 'a';
+            if((checker & (1 << val)) > 0){
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+    // space complexity is O(1) but time complexity goes up to O(n2)
+    public static boolean checkUniqueNoAdditionalDataStructures(String str){
+        for(int i = 0; i < str.length(); i++){
+            for(int j = 1 + i; j < str.length(); j++){
+                if(str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // O(n log(n)) for sorting but sorts take up more space
+    public static boolean checkUniqueSortFirst(String str){
+        str = stringSort(str);
+        for(int i = 0; i < str.length(); i++){
+            if(i == str.length() - 1) {
+                return true;
+            }
+            if(str.charAt(i) == str.charAt(1 + i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // solution 2 -------------------------------------------------------------------------------
+
+    // perhaps the best most understandable way to implement however if efficiency is very important see checkEfficientPermutation
+    public static boolean checkPermutationUsingSort(String a, String b){
+        if(a.length() != b.length()){
+            return false;
+        }
+        return stringSort(a).equals(stringSort(b));
+    }
+
+    // again we assume the char set is ASCII (always check the char set)
+    // create an int array which sets all values to 0
+    // add 1 to the array location for instance a = 97 if a was in the string twice letters[97] would equal 2
+    // then loop through the next string and subtract 1.
+    // if the value is less than 0 then there is a miss match in strings.
+    // this works as we check length first
+    public static boolean checkEfficientPermutation(String a, String b){
+        if(a.length() != b.length()){
+            return false;
+        }
+
+        int[] letters = new int[128];
+        for(int i =0; i < a.length(); i++){
+            letters[a.charAt(i)]++;
+        }
+
+        for(int i =0; i < b.length(); i++){
+            letters[b.charAt(i)]--;
+            if(letters[b.charAt(i)] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static String stringSort(String a){
+        char[] charsA = a.toCharArray();
+        Arrays.sort(charsA);
+        return new String(charsA);
+    }
+
+    // solution 3 -------------------------------------------------------------------------------
+
+    //
+    public static String urlify(char[] str, int length){
+        int numberOfSpaces = countOfChar(str, 0, length, ' ');
+        int newIndex = length - 1 + numberOfSpaces * 2;
+
+        if(newIndex + 1 < str.length) str[newIndex + 1] = '\0';
+        for(int oldIndex = length - 1; oldIndex >= 0; oldIndex -= 1){
+            if(str[oldIndex] == ' ') {
+                str[newIndex] = '0';
+                str[newIndex - 1] = '2';
+                str[newIndex - 2] = '%';
+                newIndex -= 3;
+            } else {
+                str[newIndex] = str[oldIndex];
+                newIndex -= 1;
+            }
+        }
+        return String.valueOf(str);
+    }
+
+    private static int countOfChar(char[] str, int start, int end, int target){
+        int count = 0;
+        for(int i = start; i < end; i++){
+            if(str[i] == target){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // solution 4 -------------------------------------------------------------------------------
+
+    public static void isPalindrome(String str){
+
     }
 }
--- src/test/java/ArraysAndStrings.java
+++ src/test/java/ArraysAndStrings.java
@@ -1,23 +1,96 @@
 import org.junit.jupiter.api.Test;
 
-import static org.example.Strings.checkUniqueUsingBooleanArray;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.example.Strings.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArraysAndStrings {
 
     private String strUnique = "abcdefghijklmnopqrstuvwxyz";
     private String strNotUnique = "abcdefghijklmnopabcderfghijklmnop";
+    private String permutationA = "dog";
+    private String permutationB = "god";
+    private String permutationC = "odg   ";
+    private String permutationD = "cat";
+    private char[] plainUrlText = {'M','r',' ','T','h','o','m','a','s',' ','E','y','r','e',' ',' ',' ',' '};
+    private String encodedUrlText = "Mr%20Thomas%20Eyre";
+    private String palindromeInput = "caottac";
+
+
+    // solution 1 -------------------------------------------------------------------------------
+    // Implement an algorithm to determine if all characters in a string are unique.
+    // What if you can't use additional data structures?
+
+    // first ask if its in ASCII or unicode
+    // ASCII is a 7-bit character set containing 128 characters
+    // As of Unicode version 15.1, there are 149,878 characters with code points,
+    // covering 161 modern and historical scripts, as well as multiple symbol sets.
 
     @Test
-    public void isUniqueAnswer1(){
+    public void isUnique_1_1_1(){
         assertTrue(checkUniqueUsingBooleanArray(strUnique));
         assertFalse(checkUniqueUsingBooleanArray(strNotUnique));
     }
 
     @Test
-    public void isUniqueAnswer2(){
-        assertTrue(checkUniqueUsingBooleanArray(strUnique));
-        assertFalse(checkUniqueUsingBooleanArray(strNotUnique));
+    public void isUnique_1_1_2(){
+        assertTrue(checkUniqueReducedSpaceComplexity(strUnique));
+        assertFalse(checkUniqueReducedSpaceComplexity(strNotUnique));
+    }
+
+    @Test
+    public void isUnique_1_1_3(){
+        assertTrue(checkUniqueNoAdditionalDataStructures(strUnique));
+        assertFalse(checkUniqueNoAdditionalDataStructures(strNotUnique));
+    }
+
+    @Test
+    public void isUnique_1_1_4(){
+        assertTrue(checkUniqueSortFirst(strUnique));
+        assertFalse(checkUniqueSortFirst(strNotUnique));
+    }
+
+    // solution 2 -------------------------------------------------------------------------------
+    // Given two strings, write a method to decide if one is a permutation of the other.
+
+    // first confirm some details
+    // is the permutation case-sensitive?
+    // is white space significant?
+    // for this test we will assume white space is significant, and we don't care about case sensitivity
+
+    @Test
+    public void isPermutation_1_2_1(){
+        assertTrue(checkPermutationUsingSort(permutationA, permutationB));
+        assertFalse(checkPermutationUsingSort(permutationA, permutationC));
+        assertFalse(checkPermutationUsingSort(permutationA, permutationD));
+    }
+
+    @Test
+    public void isPermutation_1_2_2(){
+        assertTrue(checkEfficientPermutation(permutationA, permutationB));
+        assertFalse(checkEfficientPermutation(permutationA, permutationC));
+        assertFalse(checkEfficientPermutation(permutationA, permutationD));
+    }
+
+    // solution 3 -------------------------------------------------------------------------------
+    // Write a method to replace all the spaces in a string with %20. You are given the string which has sufficient
+    // space at the end to hold the additional characters, so you are also given the true length of the string.
+    // example
+    // input:   "Mr John Smith     ", 13
+    // output:  "Mr%20John%20Smith"
+    // implemented with char arrays because strings are immutable
+
+    @Test
+    public void urlify_1_3_1(){
+        assertEquals(encodedUrlText, urlify(plainUrlText, 14));
+    }
+
+    // solution 4 -------------------------------------------------------------------------------
+    // Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word
+    // or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome
+    // does not need to be limited to just dictionary words. You can ignore the non-letter characters.
+
+    @Test
+    public void isPalindrome_1_4_1(){
+        isPalindrome(palindromeInput);
     }
 }
